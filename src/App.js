e52b432e92fab44a6c2aaa6f8834bbb897a30457
Abhijeet Kumar <abhijeetkumar4@deloitte.com>
2021-12-19 04:23:57 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <span> Random Message:</span>
+        <span> Random Message (Staging):</span>
         <span>{msg}</span>
 
         <button
