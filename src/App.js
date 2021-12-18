4a13eafbe76307f25efe6e42d467228fbbe02ef3
Abhijeet Kumar <abhijeetkumar4@deloitte.com>
2021-12-19 02:37:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  const getApiData = async () => {
+    // const response =await fetch("https://api.zippopotam.us/in/831015");
+    const response = await fetch("https://www.boredapi.com/api/activity");
+    const data = await response.json();
+    setMsg(data?.activity);
+    console.log(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <span> Random Message:</span>
+        <span>{msg}</span>
+
+        <button
+          style={{
+            padding: "1rem",
+            margin: ".5rem",
+            fontSize: "1.5rem",
+            backgroundColor: "#B4Bd1D",
+          }}
+          onClick={() => getApiData()}
+        >
+          Click me
+        </button>
+
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
